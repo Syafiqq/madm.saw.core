b915d41295a0e87b020728f03c5b71ef8c0e2df4
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-12 16:52:31 +0700

    Add weight container abstract class

---
+++ src/main/java/factory/WeightContainer.java
@@ -0,0 +1,12 @@
+package factory;
+
+/*
+ * This <saw> project in package <factory> created by : 
+ * Name         : syafiq
+ * Date / Time  : 10 April 2017, 8:50 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class WeightContainer
+{
+}
