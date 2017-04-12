3997218f69982ee89810ef7e77014fa0b3ea923b
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-12 16:51:14 +0700

    Add weight abstract class

---
+++ src/main/java/factory/Weight.java
@@ -0,0 +1,12 @@
+package factory;
+
+/*
+ * This <saw> project in package <factory> created by : 
+ * Name         : syafiq
+ * Date / Time  : 09 April 2017, 7:31 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Weight
+{
+}
