1c38b3e130c04fcd241a76be4cb1310b3f6cd9a4
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-12 16:53:02 +0700

    Add profit container abstract class

---
+++ src/main/java/factory/ProfitContainer.java
@@ -0,0 +1,13 @@
+package factory;
+
+/*
+ * This <saw> project in package <factory> created by : 
+ * Name         : syafiq
+ * Date / Time  : 10 April 2017, 8:22 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class ProfitContainer
+{
+    public abstract void searchProfits(Alternative alternative);
+}
